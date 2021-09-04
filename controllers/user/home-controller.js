c91fc417646704bfdf538ec6b54da17a8723a3bb
monmogo <ltvminhtuan99@gmai.com>
2021-09-04 16:44:06 +0700

    done ngay dau tien

--- controllers/user/home-controller.js
+++ controllers/user/home-controller.js
@@ -1,2 +1,7 @@
 const router = require("express").Router();
+router.get("/", (req, res) => {
+    res.render("user/home/index", { layout: "layouts/frontend-layout" });
+    // layout: "layout/backend_layout",
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const layout = require("express-ejs-layouts");
 app.set("view engine", "ejs");
 app.set("views", "./views");
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public")); //
+const homeController = require("./controllers/user/home-controller");
+//
+app.use(layout);
+app.use("/", homeController);
 app.listen(process.env.PORT, (_) => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
